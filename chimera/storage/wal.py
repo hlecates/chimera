95cd21a73d84ee9c583c9cc2b8f06057c1202e8f
Henry LeCates <hlecates26@amherst.edu>
2025-06-16 14:44:10 -0400

    Core KV operations

---
+++ chimera/storage/wal.py
@@ -0,0 +1,19 @@
+class WriteAheadLog:
+    def init__(self, path: str):
+        pass
+
+    def append(self, operation: dict):
+        pass
+
+    def replay(self):
+        pass
+
+    def rotate(self):
+        pass
+
+    def close(self):
+        pass
+    
+    def recover(self):
+        pass
+        
