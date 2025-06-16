e8efe9524ca44155859d5dc8371a93a9d21a1f0f
Henry LeCates <hlecates26@amherst.edu>
2025-06-16 16:19:42 -0400

    Notes updated.

---
+++ chimera/storage/snapshot.py
@@ -0,0 +1,12 @@
+class SnapShotManager:
+    def __init__(self, storage):
+        pass
+
+    def create(self, name, data):
+        pass
+
+    def load(self, name):
+        pass
+
+    def delete(self, name):
+        pass
