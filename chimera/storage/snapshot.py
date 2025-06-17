6e9fd4c5e16e07ba45e2bad38f5caa11d92f1a11
Henry LeCates <hlecates26@amherst.edu>
2025-06-17 19:12:15 -0400

    Snapshot manager added

--- chimera/storage/snapshot.py
+++ chimera/storage/snapshot.py
@@ -1,12 +1,30 @@
+import os
+import threading
+import pickle
+
 class SnapShotManager:
-    def __init__(self, storage):
-        pass
+    def __init__(self, path: str):
+        self.base_path = path
+        self.lock = threading.Rlock()
 
-    def create(self, name, data):
-        pass
+    def create(self, name: str, data: dict):
+        tmp = f"{self.base_path}.{name}.tmp"
+        final = f"{self.base_path}.{name}"
+        with self.lock:
+            with open(tmp, 'wb') as f:
+                pickle.dump(data, f)
+                f.flush()
+                os.fsync(f.fileno())
+            os.replace(tmp, final)
 
-    def load(self, name):
-        pass
+    def load(self, name: str) -> dict:
+        file = f"{self.base_path}.{name}"
+        if not os.path.exists(file):
+            return {}
+        with open(file , 'rb') as f:
+            return pickle.load(f)
 
     def delete(self, name):
-        pass
+        file = f"{self.base_path}.{name}"
+        if os.path.exists(file):
+            os.remove(file)
